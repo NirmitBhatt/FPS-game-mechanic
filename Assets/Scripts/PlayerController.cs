8b2f479cf7dbe4b5f730d9f7354b5aaf3b6bff4f
NirmitBhatt <nirmitb97@gmail.com>
2024-07-11 09:18:17 +0200

    Cleaned PlayerController script and added crouch mechanic

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,46 +1,96 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    float x;
-    float z;
-    public CharacterController characterController;
-    public float speed = 12f;
-    public float gravity = -9.8f;
-    public float jumpHeight = 3f;
-    public Transform groundCheck;
-    public float groundDistance = 0.4f;
-    public LayerMask groundLayerMask;
-    Vector3 velocity;
-    bool isGrounded;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    const string Horizontal = "Horizontal";
+    const string Vertical = "Vertical";
+    const string Jump = "Jump";
 
-    // Update is called once per frame
+    [SerializeField]
+    private float playerMovementSpeed = 12f;
+    [SerializeField]
+    private float gravity = -9.8f;
+    [SerializeField]
+    private float jumpHeight = 3f;
+    [SerializeField]
+    private float groundDistance = 0.4f;
+
+    private float movementAlongXAxis;
+    private float movementAlongZAxis;
+    private bool isGrounded;
+    private Vector3 velocity;
+
+    public CharacterController characterController; 
+    public Transform groundCheck;
+    public LayerMask groundLayerMask;
+    
     void Update()
     {
-        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
-        if(isGrounded && velocity.y < 0)
-        {
-            velocity.y = -2f;
-        }
-        x = Input.GetAxis("Horizontal");
-        z = Input.GetAxis("Vertical");
+        MovePlayerWASD();
+        ApplyFallGravityForPlayer();
 
-        Vector3 move = transform.right * x + transform.forward * z;
-        characterController.Move(move * speed * Time.deltaTime);
-
-        if(Input.GetButtonDown("Jump") && isGrounded)
+        if (Input.GetButtonDown(Jump) && isGrounded)
         {
-            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            PlayerJump();
         }
 
+        if (Input.GetKeyDown(KeyCode.LeftControl) && isGrounded)
+        {
+            PlayerCrouch();
+        }
+
+        if (Input.GetKeyUp(KeyCode.LeftControl))
+        {
+            ResetCharacterControllerHeight();
+        }
+    }
+
+    private void PlayerCrouch()
+    {
+        characterController.height = (characterController.height/2);
+        playerMovementSpeed = 4.5f;      
+    }
+
+    private void ResetCharacterControllerHeight()
+    {
+        characterController.height = 3.8f;
+        playerMovementSpeed = 12f;
+    }
+
+    private void ApplyFallGravityForPlayer()
+    {
         velocity.y += gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);
+        CheckIfPlayerIsOnGround();
+    }
+
+    private void PlayerJump()
+    {
+        velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+    }
+
+    private void CheckIfPlayerIsOnGround()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
+        if (isGrounded && velocity.y < 0)
+        {
+            ResetVelocity();
+        }
+    }
+
+    private void ResetVelocity()
+    {
+        velocity.y = -2f;
+    }
+
+    private void MovePlayerWASD()
+    {
+        movementAlongXAxis = Input.GetAxis(Horizontal);
+        movementAlongZAxis = Input.GetAxis(Vertical);
+        Vector3 move = transform.right * movementAlongXAxis + transform.forward * movementAlongZAxis;
+        characterController.Move(move * playerMovementSpeed * Time.deltaTime);
     }
 }
